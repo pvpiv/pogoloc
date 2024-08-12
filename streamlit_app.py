a3a95e982b35edd93333b35e182c88c933a8da83
pvpiv <fmdnyc@gmail.com>
2024-08-12 13:25:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
-from google.cloud import firestore
 import streamlit as st
+from google.cloud import firestore
+from google.oauth2 import service_account
 from datetime import datetime
 import pytz
 import json
 import re
+import time
 
 # Load Firebase credentials and create Firestore client
 key_dict = json.loads(st.secrets["textkey"])
